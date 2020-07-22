acc8960658ed76dc5280b240bf29bea4b815e94c
fwe <fwe@plutex.de>
2020-07-22 18:13:20 +0200

    add hb-trash.py

---
+++ hb-trash.py
@@ -0,0 +1,35 @@
+import requests
+import argparse
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(description="Tool to get ical or csv file")
+    action = parser.add_mutually_exclusive_group(required=True)
+    action.add_argument("-i", "--ical", action="store_true", help="ical format")
+    action.add_argument("-c", "--csv", action="store_true", help="csv")
+    # force to get an integer as value
+    parser.add_argument("-s", "--street", type=str, help="Name of street")
+    parser.add_argument("-n", "--nr", type=str, help="Street number")
+    return parser.parse_args()
+
+
+def get_data(format, args):
+    r = requests.post(
+        "https://web.c-trace.de/bremenabfallkalender/(S(bipsbtlhddavyhhnypfj2wwp))/abfallkalender/{}?abfall=".format(
+            format), params={ "strasse": str(args.street), "hausnr": str(args.nr)})
+    csv_string = r.content.decode("ISO-8859-1")
+    with open("hb-trash.{}".format("ics" if format=="cal" else format), "w") as file:
+        file.write(csv_string)
+
+
+def check_args(args):
+    if args.ical:
+        get_data("cal", args)
+    elif args.csv:
+        get_data("csv", args)
+    else:
+        raise Exception()
+
+
+if __name__ == '__main__':
+    check_args(init_argparse())
