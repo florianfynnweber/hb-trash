4896c99c4d9b97ab4dd740b596738cd590056bc9
fwe <fwe@plutex.de>
2020-07-29 16:59:35 +0200

    add new library

--- hb-trash.py
+++ hb-trash.py
@@ -1,5 +1,9 @@
+import csv
+import os
+
 import requests
 import argparse
+import pandas as pd
 
 
 def init_argparse():
@@ -11,7 +15,9 @@ def init_argparse():
     # force to get an integer as value
     parser.add_argument("-s", "--street", type=str, help="Name of street", required=True)
     parser.add_argument("-n", "--nr", type=str, help="Street number", required=True)
+    parser.add_argument("-d", "--dashboard", action="store_true")
     return parser.parse_args()
+
 
 def get_data(format, args):
     r = requests.post(
@@ -22,6 +28,19 @@ def get_data(format, args):
         file.write(csv_string)
 
 
+def writeNextPickup():
+    if os.path.exists("hb-trash.csv"):
+        df = pd.read_csv("hb-trash.csv")
+        tmp = df.to_json()
+        print(tmp)
+        with open("hb-trash.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                print(list(line))
+    else:
+        raise FileNotFoundError
+
+
 def check_args(args):
     if args.ical:
         get_data("cal", args)
@@ -29,6 +48,8 @@ def check_args(args):
         get_data("csv", args)
     else:
         raise Exception()
+    if args.dashboard:
+        writeNextPickup()
 
 
 if __name__ == '__main__':
