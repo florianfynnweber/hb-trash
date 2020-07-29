e8971de6a27764cf9d037ca66b1ea9a3b54bc9e0
fwe <fwe@plutex.de>
2020-07-29 16:32:13 +0200

    add attribute required to argument

--- hb-trash.py
+++ hb-trash.py
@@ -9,10 +9,9 @@ def init_argparse():
     action.add_argument("-i", "--ical", action="store_true", help="ical format")
     action.add_argument("-c", "--csv", action="store_true", help="csv format")
     # force to get an integer as value
-    parser.add_argument("-s", "--street", type=str, help="Name of street")
-    parser.add_argument("-n", "--nr", type=str, help="Street number")
+    parser.add_argument("-s", "--street", type=str, help="Name of street", required=True)
+    parser.add_argument("-n", "--nr", type=str, help="Street number", required=True)
     return parser.parse_args()
-
 
 def get_data(format, args):
     r = requests.post(
